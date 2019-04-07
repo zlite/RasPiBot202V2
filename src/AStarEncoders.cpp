da9d297c87d4aaa49da3d0c269feb7271f074cc8
Chris Anderson <gca2@earthlink.net>
2019-04-07 16:30:52 -0700

    Update AStarEncoders.cpp

--- src/AStarEncoders.cpp
+++ src/AStarEncoders.cpp
@@ -25,89 +25,40 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 
-/*
-An encoder class to read quadrature encoders
-on the Pololu AStar 32U4 microcontroller.
-*/
 
+#include <Encoder.h>
 #include <AStarEncoders.h>
 
-// Encoder pins
-// !!! Update ISRs and constructor below if changing these pins !!!
-// Left A: PCINT1, PB1, digital pin 15, PINB & 0B00000010 (0x02)
-const byte LEFT_A = 15;
-// Left B: PCINT2, PB2, digital pin 16, PINB & 0B00000100 (0x04)
-const byte LEFT_B = 16;
-// Right A: INT6, PE6, digital pin 7, PINE & 0B01000000 (0x40)
-const byte RIGHT_A = 7;
-// Right B: INT2, PD2, digital pin 0, PIND & 0B00000100 (0x04)
-const byte RIGHT_B = 0;
+// Change these pin numbers to the pins connected to your encoder.
+//   Best Performance: both pins have interrupt capability
+//   Good Performance: only the first pin has interrupt capability
+//   Low Performance:  neither pin has interrupt capability
+Encoder Left(1, 2);
+Encoder Right(7, 0);
 
-// Last encoder readings
-static volatile bool lastLeftA;
-static volatile bool lastLeftB;
-static volatile bool lastRightA;
-static volatile bool lastRightB;
+long positionLeft  = -999;
+long positionRight = -999;
 
 volatile uint16_t leftCount;
 volatile uint16_t rightCount;
 
-// ISRs
-// ISR for left encoder pins A & B (PCINT1, PCINT2)
-ISR(PCINT0_vect)
-{
-  bool newLeftA = PINB & 0x02; // Use direct access to PINB register bit 1
-  bool newLeftB = PINB & 0x04; // Use direct access to PINB register bit 2
 
-  leftCount += (newLeftA ^ lastLeftB) - (lastLeftA ^ newLeftB);
-
-  lastLeftA = newLeftA;
-  lastLeftB = newLeftB;
-}
-
-//ISR for right encoder pins A & B (INT6, INT2)
-static void rightISR()
-{
-  bool newRightA = PINE & 0x40; // Use direct access to PINE register bit 6
-  bool newRightB = PIND & 0x04; // Use direct access to PIND register bit 2
-
-  rightCount += (newRightA ^ lastRightB) - (lastRightA ^ newRightB);
-
-  lastRightA = newRightA;
-  lastRightB = newRightB;
-}
 
 // Constructor
 AStarEncoders::AStarEncoders()
 {
-  // Set encoder pins mode
-  pinMode(LEFT_A, INPUT_PULLUP);
-  pinMode(LEFT_B, INPUT_PULLUP);
-  pinMode(RIGHT_A, INPUT_PULLUP);
-  pinMode(RIGHT_B, INPUT_PULLUP);
-
-  // Enable PCINT1 & PCINT2; set 1 in PCMSK0 register bits 1 & 2
-  PCMSK0 |= (1 << PCINT1);
-  PCMSK0 |= (1 << PCINT2);
-
-  // Enable pin change interrupts; set 1 in PCICR register bit 1 (PCIE0)
-  PCICR |= (1 << PCIE0);
-
-  // Clear pin change interrupt flag; set 1 in PCIFR register bit 1 (PCIFR)
-  PCIFR |= (1 << PCIF0);
-
-  // Attach interrupts for right encoder
-  attachInterrupt(digitalPinToInterrupt(RIGHT_A), rightISR, CHANGE);
-  attachInterrupt(digitalPinToInterrupt(RIGHT_B), rightISR, CHANGE);
-
-  // Initialize encoders variables
-  lastLeftA = PINB & 0x02; // Use direct access to PINB register bit 4
-  lastLeftB = PINB & 0x04; // Use direct access to PINB register bit 2
-  lastRightA = PINE & 0x40; // Use direct access to PINE register bit 6
-  lastRightB = PIND & 0x04; // Use direct access to PIND register bit 2
-  leftCount = 0;
-  rightCount = 0;
-
+  Serial.println("Reading encoders");
+  leftCount = Left.read();
+  rightCount = Right.read();
+  if (leftCount != positionLeft || rightCount != positionRight) {
+    Serial.print("Left = ");
+    Serial.print(leftCount);
+    Serial.print(", Right = ");
+    Serial.print(rightCount);
+    Serial.println();
+    positionLeft = leftCount;
+    positionRight = rightCount;
+	}
   flipDirection(false, false);
 }
 
@@ -138,6 +89,7 @@ void AStarEncoders::flipDirection(bool left, bool right)
 int AStarEncoders::getCountsLeft()
 {
   cli();
+  leftCount = Left.read();
   int counts = leftCount;
   sei();
 
@@ -153,6 +105,7 @@ int AStarEncoders::getCountsLeft()
 int AStarEncoders::getCountsRight()
 {
   cli();
+  rightCount = Right.read();
   int counts = rightCount;
   sei();
 
